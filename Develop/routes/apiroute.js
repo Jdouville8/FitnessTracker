ee188f0859e3a9071a73ccbdccae7ef02783b6f2
Justin Douville <jdouville8@gmail.com>
2021-03-16 23:08:39 -0400

    Added Put and post routes as well as one range route

--- Develop/routes/apiroute.js
+++ Develop/routes/apiroute.js
@@ -1,19 +1,13 @@
 const db = require("../models");
 const router = require("express").Router();
 
-// db.Workout.find({}).then(function (res) {
-//     if (res.length === 0) {
-//         console.log("DB is populating");
-//         require("../seeders/seed.js");
-//     }
-// });
+
 
 // Gets all workouts from mongo database and returns them as an array in JSON format
 router.get("/api/workouts", (req, res) => {
 
     db.Workout.find({}).then(dbWorkout => {
         console.log("All currently existing workouts");
-        console.log(dbWorkout);
         // Iterates through array of workouts to begin calculation of total duration of combined exercises
         dbWorkout.forEach(workout => {
             // Declaration of variable to keep track of total combined exercise duration for a given workout; iteration through exercises to extract duration and add to total
@@ -22,40 +16,66 @@ router.get("/api/workouts", (req, res) => {
                 totalDur += exercise.duration;
             });
             workout.totalDuration = totalDur;
-
+            
         });
-
+        
+        console.log(dbWorkout);
         res.json(dbWorkout);
     }).catch(err => {
         res.json(err);
     });
 });
 
+// CREATE NEW WORKOUT
+router.post("/api/workouts", (req, res) => {
+    // console.log(body)
+    db.Workout.create(req.body).then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {
+        res.json(err);
+    });
+});
+
 // ADD EXERCISE
-router.put("/api/workouts/:id", ({body, params}, res) => {
-    db.Workout.findOneAndUpdate(
-        {__id: params.id},
-        {
-            $push: {exercises: body}
-        },
-        { new: true }
-    ).then(dbWorkout => {
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    )
+      .then(dbWorkout => {
         res.json(dbWorkout);
       })
       .catch(err => {
         res.json(err);
       });
+  });
+
+
+// GET WORKOUTS (within range)
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).limit(7)
+    .then((dbWorkout) => {
+        res.json(dbWorkout)
+    }).catch(err => {
+        res.json(err)
+    })
+
+
+    // db.Workout.find({}).then(dbWorkout => {
+    //     res.json(dbWorkout);
+    // }).catch(err => {
+    //     res.json(err);
+    // });
 });
-
-// CREATE NEW WORKOUT
-// router.post("/api/workouts", ({ body }, res) => {
-
-// });
-
-// GET WORKOUTS (within certain range)
-// router.get("/api/workouts/range", (req, res) => {
-
-// });
 
 
 module.exports = router;
+
+
+// db.Workout.find({}).then(function (res) {
+//     if (res.length === 0) {
+//         console.log("DB is populating");
+//         require("../seeders/seed.js");
+//     }
+// });
